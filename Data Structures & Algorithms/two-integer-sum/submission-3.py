6cd6c72d7f2a82c740e42bc4540867dd4286cf18
Lokesh Kumar <67655248+lokeshkumar9600@users.noreply.github.com>
2026-08-02 11:57:07 +0100

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashset = {}
+        for i in range(len(nums)):
+            if(target - nums[i] in hashset):
+                return [hashset[target-nums[i]],i]
+
+            if nums[i] not in hashset:
+                hashset[nums[i]] = hashset.get(nums[i],i)
+            
+        
+            
+        
